41d8dd92c9ff06a8f87d9cd4ac17951423a3585c
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-25 10:21:26 +0530

    Add api

--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from .serializers import QuestionSerializer
+from rest_framework import generics
 
 
-def index(request):
-    questions = Question.objects.all()
-    for question in questions:
-        question.choices = question.choice_set.all()
-    context = {
-        'questions': questions,
-    }
-    return render (request,'quiz.html', context)
+class GetQuestions(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer

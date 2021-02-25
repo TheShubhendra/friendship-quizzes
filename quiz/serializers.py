d31c92720290ae40ef6f083f9b09a3e2269ae485
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-25 10:04:24 +0530

    Add serializer

---
+++ quiz/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Question, Choice
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ('question_text', 'question_id')

b13f6d3cd5abbc9701d1b7d51c96dcfc100b5c7d
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-03-09 16:50:11 +0530

    Add new app Frienship Questions

---
+++ friendshipQuestions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    question = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="options"
+    )
+    choice_id = models.AutoField(primary_key=True)
+    choice = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.option

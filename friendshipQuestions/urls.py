76be6565789f9599e9bdf5e3df10cf8b49eb56b4
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-03-09 17:56:36 +0530

    Fix url rounting

--- friendshipQuestions/urls.py
+++ friendshipQuestions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.FriendshipQuestionList.as_view(), name="Friendship Questions"),
+    path("questions/", views.FriendshipQuestionList.as_view(), name="Friendship Questions"),
 ]
